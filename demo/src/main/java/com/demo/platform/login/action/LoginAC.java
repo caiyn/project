4dbcb0a8897271714cfa704c283f19291a1a95d5
LiJing <1018480482@qq.com>
2018-01-09 12:00:30 +0800

    commit loginAc

--- demo/src/main/java/com/demo/platform/login/action/LoginAC.java
+++ demo/src/main/java/com/demo/platform/login/action/LoginAC.java
@@ -5,16 +5,39 @@ import com.demo.common.action.CommonAction;
 import com.demo.platform.login.bizservice.LoginBS;
 import com.demo.platform.login.domain.User;
 
+
 public class LoginAC extends CommonAction {
 	private LoginBS loginBS;
 	private static final long serialVersionUID = 1L;
 	
 	private User user;
-	
+	private String username;
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	public String registerUser(){
-		loginBS.registerUser(user);
+		loginBS.registerUser(username,password);
 		return SUCCESS;
 	}
+//	public String execute(){
+//		loginBS.registerUser(user);
+//		return SUCCESS;
+//	}
 	
 	public void setLoginBS(LoginBS loginBS) {
 		this.loginBS = loginBS;
