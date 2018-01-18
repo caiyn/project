02317102ddb7d51eb1ab8247d17395520611b201
LiJing <1018480482@qq.com>
2018-01-18 14:40:54 +0800

    commit pagination Domain

---
+++ demo/src/main/java/com/demo/common/domain/Pagination.java
@@ -0,0 +1,40 @@
+package com.demo.common.domain;
+
+public class Pagination {
+	private int start = 0;
+	private int limit = 0;
+	private int total = 0;
+	private int totalCount = 0;
+	private int pageCount = 0;
+	public int getStart() {
+		return start;
+	}
+	public void setStart(int start) {
+		this.start = start;
+	}
+	public int getLimit() {
+		return limit;
+	}
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public int getTotalCount() {
+		return totalCount;
+	}
+	public void setTotalCount(int totalCount) {
+		this.totalCount = totalCount;
+	}
+	public int getPageCount() {
+		return pageCount;
+	}
+	public void setPageCount(int pageCount) {
+		this.pageCount = pageCount;
+	}
+	
+}
